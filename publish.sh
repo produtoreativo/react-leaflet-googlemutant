62c1baa817a5721d647a3c02c1d02134c38cb367
Christiano Milfont <cmilfont@gmail.com>
2017-06-09 14:05:44 -0300

    0.1.4

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -x
 
-#yarn release
+yarn release
 VERSION=`node -e "console.log(require('./package.json').version);"`
 NODE_ENV=production __VERSION__=$VERSION yarn transpile
-# git commit -a -m $VERSION
-# git push --follow-tags origin master
-# npm publish
+git commit -a -m $VERSION
+git push --follow-tags origin master
+npm publish
